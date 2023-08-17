41a5f78edf9fc664fe1b68073fb4f57dbd5db79a
JakeLiang0610 <jake.liang0610@gmail.com>
2023-08-17 18:54:48 +0800

    0817 first push

---
+++ README.md
@@ -0,0 +1,12 @@
+# 適用於 Git Hub Organization 的打包下載懶人腳本
+
+### 設定好access Token後 再main下方輸入相關資料 即可一次下載所有專案
+
+* username 是註冊帳號的mail 不是暱稱
+* access_token 申請的Token
+* organization_name organization 名稱
+
+###### 2023/08/17 
+
+    1.初版
+    2.Git lab版本製作中
---
+++ git_hub_downloader.py
@@ -0,0 +1,66 @@
+import os
+import requests
+import subprocess
+import threading
+
+
+def get_repositories_from_github(org_name, token):
+
+    repo_list_url = f"https://api.github.com/orgs/{org_name}/repos?per_page=100"
+    headers = {"Authorization": f"token {token}"}
+
+    all_repos = []
+    while repo_list_url:
+        response = requests.get(repo_list_url, headers=headers)
+        repos = response.json()
+
+        for repo in repos:
+            project_name = repo["name"]
+            clone_url = repo["clone_url"]
+            all_repos.append((project_name, clone_url))
+
+        link_header = response.headers.get("Link", "")
+        next_page_url = None
+        if link_header:
+            links = link_header.split(", ")
+            for link in links:
+                url, rel = link.split("; ")
+                if rel == 'rel="next"':
+                    next_page_url = url.strip("<>")
+        repo_list_url = next_page_url
+
+    return all_repos
+
+
+def clone_repository(project_name, repo_url, t_folder):
+    target_path = os.path.join(t_folder, project_name)
+    command = ["git", "clone", repo_url, target_path]
+    subprocess.run(command)
+
+
+def clone_repositories_to_local(t_folder, repo_list):
+    threads = []
+
+    for project_name, repo_url in repo_list:
+        t = threading.Thread(target=clone_repository, args=(project_name, repo_url, t_folder))
+        t.start()
+        threads.append(t)
+
+    # Wait for all threads to complete
+    for t in threads:
+        t.join()
+
+
+if __name__ == '__main__':
+
+    t_folder = ""  # save path name
+    os.makedirs(t_folder, exist_ok=True)
+
+    username = "JakeLiang0610"  # git hub mail not nickname
+    access_token = "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"  # access token
+    organization_name = ""  # organization_name
+
+    repositories = get_repositories_from_github(organization_name, access_token)
+    clone_repositories_to_local(t_folder, repositories)
+
+    print("fin")
